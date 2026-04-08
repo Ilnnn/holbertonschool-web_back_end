18ca907da5b04142c06af04513708898c74f2926
Ilnnn <camoreze@gmail.com>
2026-04-08 11:44:24 +0200

    3-tasks.py

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import time
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+
+    return asyncio.Task
