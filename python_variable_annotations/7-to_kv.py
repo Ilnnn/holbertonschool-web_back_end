7f5c2d315a8dc6f1f005d459d15f95b71338fc6f
Ilnnn <camoreze@gmail.com>
2026-04-08 10:28:09 +0200

    7-to_kv.py

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python3
+"""Module that provides a type-annotated function to_kv."""
 from typing import Union, Tuple
 
-def to_kv(k: str, v: Union(int, float)) -> Tuple(str, float):
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Return a tuple consisting of k and the square of v as a float."""
     return (k, float(v**2))
