f2be566332753bfaae41281e811945eda3e88a6c
Ilnnn <camoreze@gmail.com>
2026-04-08 10:16:59 +0200

    1-concat.py

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,2 +1,7 @@
+#!/usr/bin/env python3
+"""Module with a type-annotated function that concatenates two strings."""
+
+
 def concat(str1: str, str2: str) -> str:
+    """Concatenate two strings and return the result."""
     return str1 + str2
