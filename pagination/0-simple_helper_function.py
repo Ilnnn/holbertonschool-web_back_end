9cc8c7196ac2c2f1a41e4baba85575ede5a71ade
Ilnnn <camoreze@gmail.com>
2026-04-20 10:30:37 +0200

    shebang, docstring and type tuple add

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,4 +1,8 @@
-def index_range(page: int, page_size: int) -> tuple:
+#!/usr/bin/env python3
+"""Pagination function."""
+from typing import Tuple
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Calculate the start and end index for pagination.
     
