f1584ebacdc02d33852f3becbaab998dc06206b1
Ilnnn <camoreze@gmail.com>
2026-04-21 10:49:02 +0200

    10-update_topics.py

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,13 +1,14 @@
+#!/usr/bin/env python3
+"""
+Provides a utility function to interact with a MongoDB collection.
+"""
+
+
 def update_topics(mongo_collection, name, topics):
     """
-    Updates all topics of a school document based on the name.
-
-    Args:
-        mongo_collection: A pymongo collection object.
-        name (str): The school name to search for.
-        topics (list of str): The list of topics to set for the school.
+    Changes all topics of a school document based on the name.
     """
     mongo_collection.update_many(
-        { "name": name },
-        { "$set": { "topics": topics } }
+        {"name": name},
+        {"$set": {"topics": topics}}
     )
