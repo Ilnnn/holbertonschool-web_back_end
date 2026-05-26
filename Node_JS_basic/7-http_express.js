72956183050e0c51627137e45dfba96fdf069cdd
Ilnnn <camoreze@gmail.com>
2026-05-26 10:32:53 +0200

    Fix: Prepend header to error message in /students endpoint

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -54,7 +54,7 @@ app.get('/students', (req, res) => {
       res.send(`This is the list of our students\n${output}`);
     })
     .catch((error) => {
-      res.status(500).send(error.message);
+      res.status(500).send(`This is the list of our students\n${error.message}`);
     });
 });
 
