8bb41bc800a4051648ead6e19b99042ef1245b60
Ilnnn <camoreze@gmail.com>
2026-05-26 10:29:39 +0200

    Add 7-http_express.js: Complex Express server with student data

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const students = lines.slice(1);
+
+      const fields = {};
+      let output = `Number of students: ${students.length}\n`;
+
+      students.forEach((student) => {
+        const details = student.split(',');
+        if (details.length === 4) {
+          const firstname = details[0];
+          const field = details[3];
+
+          if (!fields[field]) {
+            fields[field] = [];
+          }
+          fields[field].push(firstname);
+        }
+      });
+
+      for (const field in fields) {
+        if (Object.prototype.hasOwnProperty.call(fields, field)) {
+          output += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
+        }
+      }
+      resolve(output);
+    });
+  });
+}
+
+const dbFile = process.argv[2];
+
+app.get('/', (req, res) => {
+  res.set('Content-Type', 'text/plain');
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  res.set('Content-Type', 'text/plain');
+  countStudents(dbFile)
+    .then((output) => {
+      res.send(`This is the list of our students\n${output}`);
+    })
+    .catch((error) => {
+      res.status(500).send(error.message);
+    });
+});
+
+app.listen(1245);
+
+module.exports = app;
